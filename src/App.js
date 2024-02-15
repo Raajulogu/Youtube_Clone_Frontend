942d7e0fade14fc67866725a79a20d37a58b57b9
Rajeshkumar_145 <rajeshkumarlogu145@gmail.com>
2024-02-15 15:42:15 +0530

    anitha update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route, Routes } from 'react-router';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import ResetPassword from './Pages/ResetPassword';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='login' element={<Login/>}/>
-        <Route path='signup' element={<SignUp/>}/>
-        <Route path='reset-password' element={<ResetPassword/>}/>
+        <Route exact path='/' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
       </Routes>
     </div>
   );
